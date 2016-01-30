130e6c8247c4a5b9e2345598b5a5b3f43033343b
Morgante Pell <morgante.pell@morgante.net>
2016-01-31 03:04:37 +0400

    old work

--- profit.py
+++ profit.py
@@ -1,11 +1,9 @@
 from __future__ import division
 import random
+import numpy
 
 odds = 0.49
-min = 5
-wallet = 5000
-rounds = (100 * 24)
-goal = wallet + 75
+rounds = (100 * 48)
 
 def sim(odds, min, wallet, rounds, goal):
 	round = 1
@@ -24,14 +22,39 @@ def sim(odds, min, wallet, rounds, goal):
 
 	return wallet
 
-won = 0
-sims = 1000
-for i in range(0,sims):
-	result = sim(odds, min, wallet, rounds, goal)
+def test(odds, min, wallet, rounds, goal):
+	won = 0
+	sims = 1000
+	returns = []
+	for i in range(0,sims):
+		result = sim(odds, min, wallet, rounds, goal)
 
-	if (result >= goal):
-		won += 1
-		print "In simulation %d, won %d" % (i, result)
+		returns.append(result)
 
-percent = won / sims
-print "Reached $%d in %d of %d sims (%f)" % (goal, won, sims, percent)
+		if (result >= goal):
+			won += 1
+			# print "In simulation %d, won %d" % (i, result)
+
+	percent = won / sims
+	ev = numpy.mean(returns)
+	# print "Reached $%d in %d of %d sims (%f)" % (goal, won, sims, percent)
+
+	# print "Expected return: %d" % (ev)
+
+	return ev
+
+success = []
+
+for bet in range(10, 1000, 10):
+	for i in range(1, 50):
+		wallet = bet * i
+		goal = wallet + bet
+
+		ev = test(odds, bet, wallet, rounds, goal)
+
+		print "Expected return on %d: %d" % (wallet, ev)
+
+		if (ev > wallet):
+			success.append((bet, wallet))
+
+print success
